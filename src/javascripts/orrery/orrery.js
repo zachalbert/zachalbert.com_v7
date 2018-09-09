e1bb27a2839b13125cfeeeac1189c80bfa726f68
zachalbert <zachalbert@gmail.com>
2018-09-09 13:32:47 -0700

    Cleanup orbits and responsive tuning

--- src/javascripts/orrery/orrery.js
+++ src/javascripts/orrery/orrery.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
+  let minScreenWidth="768";
+  let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-  /* Behavior onLoad */
-  universePlay();
+  /* Determine the universe state immediately on load… */
+  init( w );
+
+  /* …and everytime there's a screen resize. */
+  $(window).resize( function() {
+    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    init( w );
+  });
+
+  /* Set the state of the universe player */
+  function init( screenWidth ) {
+    let wideEnough = screenWidth > minScreenWidth ? true : false;
+    if( wideEnough ) {
+      universePlay();
+      $('.universe-controls').show();
+    } else {
+      universePause();
+      $('.universe-controls').hide();
+    }
+  }
 
   $('#toggle-pause').click( function(e) {
     e.preventDefault();
@@ -28,38 +48,38 @@ $(document).ready(function() {
     togglePluto();
   })
 
-  function universePlay() {
-    $('.universe').removeClass('pause fast');
-    $('.universe').addClass('play');
-
-    $('.universe-controls_player').removeClass('active');
-    $('.control-play').addClass('active');
-  }
-
-  function universePause() {
-    $('.universe').removeClass('play fast');
-    $('.universe').addClass('pause');
-
-    $('.universe-controls_player').removeClass('active');
-    $('.control-pause').addClass('active');
-  }
-
-  function universeFast() {
-    $('.universe').removeClass('pause');
-    $('.universe').addClass('play fast');
-
-    $('.universe-controls_player').removeClass('active');
-    $('.control-fast').addClass('active');
-  }
-
-  function toggleLabels() {
-    $('.universe').toggleClass('labels-on');
-
-    $('.control-labels').toggleClass('active');
-  }
-
-  function togglePluto() {
-    $('.pluto').toggle();
-  }
-
 });
+
+function universePlay() {
+  $('.universe').removeClass('pause fast');
+  $('.universe').addClass('play');
+
+  $('.universe-controls_player').removeClass('active');
+  $('.control-play').addClass('active');
+}
+
+function universePause() {
+  $('.universe').removeClass('play fast');
+  $('.universe').addClass('pause');
+
+  $('.universe-controls_player').removeClass('active');
+  $('.control-pause').addClass('active');
+}
+
+function universeFast() {
+  $('.universe').removeClass('pause');
+  $('.universe').addClass('play fast');
+
+  $('.universe-controls_player').removeClass('active');
+  $('.control-fast').addClass('active');
+}
+
+function toggleLabels() {
+  $('.universe').toggleClass('labels-on');
+
+  $('.control-labels').toggleClass('active');
+}
+
+function togglePluto() {
+  $('.pluto').toggle();
+}
